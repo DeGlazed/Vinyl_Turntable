a43c60604c87eee65e20b54bc5cfebd2eaf9155a
DeGlazed <raducutanase@gmail.com>
2022-05-28 12:38:34 +0300

    finish sw

--- sketch_may19a/sketch_may19a.ino
+++ sketch_may19a/sketch_may19a.ino
@@ -1,7 +1,9 @@
 #include <SoftwareSerial.h>
 #include <util/atomic.h>
 #define ENCA 2
-#define PWM_PIN 5
+#define ENCB 3
+#define PWM_PINA 5
+#define PWM_PINB 6
 
 const byte rxpin = 8;
 const byte txpin = 9;
@@ -20,43 +22,34 @@ float vPrev = 0;
 
 float eintegral = 0;
 
+float vt = 0;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   myserial.begin(9600);
-  pinMode(PWM_PIN, OUTPUT);
+  pinMode(PWM_PINA, OUTPUT);
+  pinMode(PWM_PINB, OUTPUT);
   pinMode(ENCA, INPUT);
+  pinMode(ENCB, INPUT);
 
   attachInterrupt(digitalPinToInterrupt(ENCA), readEncoder, RISING);
 }
 
 void loop() {
-/* 
-// put your main code here, to run repeatedly:
-//for some reason 255 = 0; ~85 = full power
- if(myserial.available()) {
-   w = myserial.read();
-   switch(w) {
-    case '0': 
-      pwm = 0;
-      break;
-    case '1': pwm = 20; 
-      break;
-    case '2': pwm = 40; 
-      break;
-    case '3': pwm = 60; 
-      break;
-   }
-   Serial.write(w);
- }
-//
-// if(Serial.available()) {
-//   w = Serial.read();
-//   myserial.write(w);
-// }
- */
-int pos = 0;
 
+if(myserial.available()){
+  char w = myserial.read();
+  switch(w) {
+    case '0': vt = 0.0; break;
+    case '1': vt = 33.0; break;
+    case '2': vt = 45.0; break;
+    case '3': vt = 78.0; break;
+  }
+  Serial.println(w);
+  Serial.println(vt);
+}
+  
+int pos = 0;
 ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
   pos = pos_i;
 }
@@ -64,6 +57,8 @@ ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
 long currT = micros();
 float deltaT = ((float) (currT- prevT))/1.0e6;
 float velocity = (pos - posPrev)/deltaT;
+posPrev = pos;
+prevT = currT;
 
 //switch to RPM
 float v = velocity/360.0*60.0;
@@ -72,36 +67,42 @@ float v = velocity/360.0*60.0;
 vFilt = 0.854*vFilt + 0.0728*v + 0.0728*vPrev;
 vPrev = v;
 
-//target
-float vt = 10.0;
-
 //control signal
-float kp = 3;
-float ki = 1;
+float kp = 5;
+float ki = 10;
 float e = vt - vFilt;
 eintegral = eintegral + e * deltaT;
 float u = kp * e + ki * eintegral;
 
+int dir = 1;
 if(u < 0)
-  pwm = 0;
+  dir = -1;
 pwm = (int) fabs(u);
-if(pwm > 255){
+if(pwm > 255)
   pwm = 255;
-}
 
-setMotor(255 - pwm, PWM_PIN);
+setMotor(dir, pwm);
 
-Serial.println(v);
+//Serial.println(v);
 //for consistent sampling
 delay(1);
 }
 
-void setMotor(int pwmVal, int pwmPin)
+void setMotor(int dir, int pwmVal)
 {
-  analogWrite(pwmPin, pwmVal);  
+  if(dir == 1)
+    analogWrite(PWM_PINB, pwmVal); 
+  if(dir == -1)
+    analogWrite(PWM_PINA, pwmVal);
 }
 
 void readEncoder()
 {
-   pos_i++;
+  int b = digitalRead(ENCB);
+  int increment = 0;
+  if(b > 0)
+    increment = 1;
+  else
+    increment = -1;
+   pos_i = pos_i + increment;
 }
